1d7092329c119d84919b559ccfbba442a50fd8f1
Mathias Bonnet <mathias.bonnet@zivver.com>
2016-12-11 23:29:32 +0100

    install and add fastlane

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -23,14 +23,6 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
-    },
-    {
-        "id": "cordova-universal-links-plugin.universalLinks",
-        "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
-        "pluginId": "cordova-universal-links-plugin",
-        "clobbers": [
-            "universalLinks"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -40,8 +32,7 @@ module.exports.metadata =
     "cordova-plugin-console": "1.0.4",
     "cordova-plugin-crosswalk-webview": "2.2.0",
     "cordova-plugin-statusbar": "2.2.0",
-    "cordova-plugin-whitelist": "1.3.0",
-    "cordova-universal-links-plugin": "1.2.1"
+    "cordova-plugin-whitelist": "1.3.0"
 };
 // BOTTOM OF METADATA
 });
